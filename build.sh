448257a8aab20ad5b20aa7ea1ab0bafe5289d4e5
Zsolt Tasnadi <rastasi@gmail.com>
2025-12-03 19:35:00 +0100

    remove static info and icon files

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ git clone --recursive https://github.com/nesbox/TIC-80  Contents/Resources/src
 cd Contents/Resources/src/build
 cmake -DBUILD_WITH_ALL=On -DBUILD_PRO=On -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
 make -j4
+cp macosx/tic80.icns ../../tic80.icns
+cp macosx/tic80.plist ../../../Info.plist
